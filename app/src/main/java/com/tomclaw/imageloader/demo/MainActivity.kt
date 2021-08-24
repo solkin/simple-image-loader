7393a42b5de3784e887ea525ec015c3698ef6e87
Igor Solkin <inbox@tomclaw.com>
2021-08-24 22:39:05 +0300

    Add custom loader handlers as extensions

--- app/src/main/java/com/tomclaw/imageloader/demo/MainActivity.kt
+++ app/src/main/java/com/tomclaw/imageloader/demo/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.tomclaw.imageloader.demo
 
-import android.graphics.Bitmap
-import android.graphics.PorterDuff
 import android.os.Bundle
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.tomclaw.imageloader.SimpleImageLoader
+import com.tomclaw.imageloader.centerCrop
+import com.tomclaw.imageloader.centerInside
+import com.tomclaw.imageloader.errorResWithTint
+import com.tomclaw.imageloader.fitCenter
+import com.tomclaw.imageloader.placeholderResWithTint
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,58 +25,22 @@ class MainActivity : AppCompatActivity() {
 
         SimpleImageLoader.load()
             .from("https://zibuhoker.ru/ifm/images/image1.jpg")
-            .placeholderHandler(centerResWithTint(R.drawable.ic_image, R.color.teal_700))
-            .errorHandler(centerResWithTint(R.drawable.ic_image_remove, R.color.purple_500))
-            .successHandler(centerCrop)
+            .placeholderResWithTint(R.drawable.ic_image, ContextCompat.getColor(this, R.color.teal_700))
+            .errorResWithTint(R.drawable.ic_image_remove, R.color.purple_500)
+            .centerCrop()
             .into(image1)
         SimpleImageLoader.load()
             .from("https://zibuhoker.ru/ifm/images/image2.jpg")
-            .placeholderHandler(centerResWithTint(R.drawable.ic_image, R.color.teal_700))
-            .errorHandler(centerResWithTint(R.drawable.ic_image_remove, R.color.purple_500))
-            .successHandler(fitCenter)
+            .placeholderResWithTint(R.drawable.ic_image, R.color.teal_700)
+            .errorResWithTint(R.drawable.ic_image_remove, R.color.purple_500)
+            .fitCenter()
             .into(image2)
         SimpleImageLoader.load()
             .from("https://zibuhoker.ru/ifm/images/image3.jpg")
-            .placeholderHandler(centerResWithTint(R.drawable.ic_image, R.color.teal_700))
-            .errorHandler(centerResWithTint(R.drawable.ic_image_remove, R.color.purple_500))
-            .successHandler(centerInside)
+            .placeholderResWithTint(R.drawable.ic_image, R.color.teal_700)
+            .errorResWithTint(R.drawable.ic_image_remove, R.color.purple_500)
+            .centerInside()
             .into(image3)
     }
 
-    private val fitCenter: (ImageView, Bitmap) -> Unit = { imageView, bitmap ->
-        with(imageView) {
-            scaleType = ImageView.ScaleType.FIT_CENTER
-            colorFilter = null
-            setImageBitmap(bitmap)
-        }
-    }
-
-    private val centerCrop: (ImageView, Bitmap) -> Unit = { imageView, bitmap ->
-        with(imageView) {
-            scaleType = ImageView.ScaleType.CENTER_CROP
-            colorFilter = null
-            setImageBitmap(bitmap)
-        }
-    }
-
-    private val centerInside: (ImageView, Bitmap) -> Unit = { imageView, bitmap ->
-        with(imageView) {
-            scaleType = ImageView.ScaleType.CENTER_INSIDE
-            colorFilter = null
-            setImageBitmap(bitmap)
-        }
-    }
-
-    private val centerResWithTint = fun(drawableRes: Int, tintColor: Int): (ImageView) -> Unit {
-        return {
-            with(it) {
-                scaleType = ImageView.ScaleType.CENTER
-                setImageResource(drawableRes)
-                setColorFilter(
-                    ContextCompat.getColor(context, tintColor),
-                    PorterDuff.Mode.MULTIPLY
-                )
-            }
-        }
-    }
 }
