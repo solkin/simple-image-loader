2aa89013ae4ccb959ccf1bcf98e7b8e832993f98
Igor Solkin <inbox@tomclaw.com>
2021-09-01 18:04:04 +0300

    Update demo app to show GitHub users with avatars

--- app/src/main/java/com/tomclaw/imageloader/demo/MainActivity.kt
+++ app/src/main/java/com/tomclaw/imageloader/demo/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.tomclaw.imageloader.demo
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.tomclaw.imageloader.SimpleImageLoader.imageLoader
+import java.net.URL
+import kotlin.concurrent.thread
+import kotlin.random.Random
+import org.json.JSONArray
 
 class MainActivity : AppCompatActivity() {
 
+    @SuppressLint("NotifyDataSetChanged")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,20 +22,29 @@ class MainActivity : AppCompatActivity() {
 
         val recyclerview = findViewById<RecyclerView>(R.id.recycler)
         recyclerview.layoutManager = LinearLayoutManager(this)
+
+
         val data = ArrayList<ItemsViewModel>()
-        val uris = listOf(
-            "https://zibuhoker.ru/ifm/images/image1.jpg",
-            "https://zibuhoker.ru/ifm/images/image2.jpg",
-            "https://zibuhoker.ru/ifm/images/image3.jpg",
-            "https://zibuhoker.ru/ifm/images/image4.jpg",
-            "file:///android_asset/image1.jpg",
-        )
-        for (i in 1..30) {
-            val imageIndex = i % uris.size
-            val uri = uris[imageIndex]
-            data.add(ItemsViewModel(uri, "Image $i", uri))
-        }
         val adapter = CustomAdapter(data)
+
+        thread {
+            // Yeah, this demo is quite a codeshit :)
+            try {
+                val since = Random(System.currentTimeMillis()).nextInt(100, 5000)
+                val json = URL("https://api.github.com/users?since=$since").readText()
+                val array = JSONArray(json)
+                for (i in 0 until array.length()) {
+                    val obj = array.getJSONObject(i)
+                    val avatarUrl = obj.optString("avatar_url") ?: continue
+                    val login = obj.optString("login")
+                    val url = obj.optString("url")
+                    data.add(ItemsViewModel(avatarUrl, login, url))
+                }
+            } catch (ignored: Throwable) {
+            }
+            runOnUiThread { adapter.notifyDataSetChanged() }
+        }
+
         recyclerview.adapter = adapter
     }
 
